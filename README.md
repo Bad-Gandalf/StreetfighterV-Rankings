f60bee35f4872e0d02bd8db2a49819225582b621
Patrick Doherty <patrick.e.doherty@gmail.com>
2018-09-24 11:45:17 +0000

    Duplicate and unneccessary code deleted, more comments added and more details added to readme

--- README.md
+++ README.md
@@ -1,18 +1,76 @@
+# Milestone Project 2 - StreetFighter 5 Top 50 Professionals Dashboard
+#### by Patrick Doherty
 
-     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
-    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
-    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
-    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
-     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
-    ----------------------------------------------------------------- 
+## CREATE A DATA DASHBOARD
+
+The brief given for this project was:
+- Build a data dashboard that visualizes a dataset of your choice
+- Your data can be stored locally (e.g., in a js file) or sourced from an API
+- Visualise your data using D3.js and dc.js
+ 
+The dataset of my choice is from Capcom Pro Tour detailing the participants. This is based on the 
+professional e-sport StreetFighter 5.
+The data is stored locally on a csv file after being ripped from a website detailing the scores. 
+I have used D3.js and dc.js to create a dashboard displaying charts regarding the Capcom Pro Tour. 
+
+This information could be interesting to the players and the fans of the Fighting Game Community. By detailing
+statistics such as these, people will have hard facts to help them decide upon which character to choose.
+With more work it can also help the community track player form, season to season taking into account patches
+for the game.
+
+## UX
+
+User wished to check statistics based on a particular variable, ie country, gender, team. They click on this 
+variable in the form of a bar/slice of a chart and instantly we can see the rest of the charts adjust for that 
+specific variable. 
+Alot of users will struggle to make sense of these charts by just looking at them and reading the outputs. I have
+included descriptions of what is happening beside or below each of the charts.
+Even with descriptions attached to the charts this may not be enough. The user can click on "Start the Tour" and
+they will be guided around each chart by intro.js. To further explain the features the dashboard possesses,
+the tour will explain fow to use these features such as clicking on bar/slices to isolate a particular variable,
+as previously mentioned.
 
 
-Hi there! Welcome to Cloud9 IDE!
+## Technolgies used:
 
-To get you started, create some files, play with the terminal,
-or visit http://docs.c9.io for our documentation.
-If you want, you can also go watch some training videos at
-http://www.youtube.com/user/c9ide.
+- HTML, CSS, Javascript 
+- [Bootstrap](https://getbootstrap.com/)
+    - Used for its grid system to help in design going from mobile to larger displays.
+- [crossfilter](https://github.com/crossfilter/crossfilter)
+    - to parse and compare data
+- [d3](https://d3js.org/)
+    - to create charts from data
+- [dc](https://dc-js.github.io/dc.js/)
+    - to create charts from data
+- [JQuery](https://jquery.com)
+    - The project uses **JQuery** to simplify DOM manipulation.
+- [queue](https://github.com/d3/d3-queue)
+    - This allows us to await collection of data before running scripts.
+- [intro.js](https://github.com/usablica/intro.js)
+    - For guided tour of charts with explanations.
 
-Happy coding!
-The Cloud9 IDE team
+## Testing
+
+- I carried out testing based on the UX section above. 
+    - Select different bars/slices across the page to check for interactions
+    - Take the tour. I followed the tour from start to finish testing all the buttons.
+    - I took the tour backwards, making sure I checked every button that could be pressed.
+    - I printed to the console any data that had been parsed and compared it to the csv file.
+    - During development I checked for any errors in the console and eventually resolved all.
+    
+
+
+## The Future
+
+
+## Deployment
+
+
+
+## Credits
+
+### Content
+- 
+
+### Media
+- 
--- static/js/graph.js
+++ static/js/graph.js
@@ -1,12 +1,12 @@
-var StreetFighterApi = function() {
-// Await loading of csv file with data and then begin to make the graphs
+const StreetFighterApi = function() {
+ // Await loading of csv file with data and then begin to make the graphs
  this.init = function() {
   queue()
    .defer(d3.csv, "/data/StreetfighterVrankings.csv")
    .await(this.makeGraphs);
  };
 
- this.makeGraphs = function (error, data) {
+ this.makeGraphs = function(error, data) {
   var ndx = crossfilter(data);
 
   //Data Parsing
@@ -28,15 +28,17 @@ var StreetFighterApi = function() {
   });
   console.log(data);
 
- function add_item(p, v) {
-   // For each different character, count their occurences and total their 'Lifetime score'
+  // Building custom reducers to get correct averages.
+  function add_item(p, v) {
+   // For each different character/gender etc, count their occurences and total their 'Lifetime score'
    // Then find its average lifetime score. Return an object with count, total and average values.
+   // i.e. Add a fact
    p.count++;
    p.total += v["Lifetime Score"];
    p.average = p.total / p.count;
    return p;
   }
-// Removes the fact thats been added
+  // Removes the fact thats been added previously
   function remove_item(p, v) {
    p.count--;
    if (p.count == 0) {
@@ -49,179 +51,181 @@ var StreetFighterApi = function() {
    }
    return p;
   }
-// Sets the initial value.
+  // Sets the initial value.
   function initialise() {
    return { count: 0, total: 0, average: 0 };
   }
 
 
- // Actual Score versus Lifetime Rank Scatter Plot
- this.show_lifetime_rank_to_actual_scores_correlation = function(ndx) {
-// Choosing the colours each country that is represented in the chart.
-  var countryColors = d3.scale.ordinal()
-   .domain(["Japan", "United States", "Republic of Korea", "Taiwan", "United Kingdom", "France", "Singapore", "China", "Dominican Republic", "Belgium", "Norway"])
-   .range(["black", "blue", "red", "yellow", "orange", "grey", "green", "pink", "purple", "brown", "light-blue"]);
-// Use "Rank" as the X-axis and select other values to be associated with it.
-  var rDim = ndx.dimension(dc.pluck("Rank"));
-  var rankDim = ndx.dimension(function(d) {
-   return [d.Rank, d["Actual Score"], d.Name, d.Country, d.Character];
-  });
-// Group these details by the the rank/ individual
-  var actualScoresDim = rankDim.group();
-// Find the top and bottom rank figures in order to help declare a range for the x-axis
-  var minRank = rDim.bottom(1)[0].Rank;
-  var maxRank = rDim.top(1)[0].Rank;
+  // Actual Score versus Lifetime Rank Scatter Plot
+  this.show_lifetime_rank_to_actual_scores_correlation = function(ndx) {
+   // Choosing the colours each country that is represented in the chart.
+   var countryColors = d3.scale.ordinal()
+    .domain(["Japan", "United States", "Republic of Korea", "Taiwan", "United Kingdom", "France", "Singapore", "China", "Dominican Republic", "Belgium", "Norway"])
+    .range(["black", "blue", "red", "yellow", "orange", "grey", "green", "pink", "purple", "brown", "light-blue"]);
+   // Use "Rank" as the X-axis and select other values to be associated with it.
+   var rDim = ndx.dimension(dc.pluck("Rank"));
+   var rankDim = ndx.dimension(function(d) {
+    return [d.Rank, d["Actual Score"], d.Name, d.Country, d.Character];
+   });
+   // Group these details by the the rank/ individual
+   var actualScoresDim = rankDim.group();
+   // Find the top and bottom rank figures in order to help declare a range for the x-axis
+   var minRank = rDim.bottom(1)[0].Rank;
+   var maxRank = rDim.top(1)[0].Rank;
 
-// Plot a scatter graph
-  dc.scatterPlot("#Lifetime-rank-to-actual-score")
-   .width(1000) 
-   .height(300)
-   .x(d3.scale.linear().domain([minRank, maxRank])) // x-axis scale depending upon min and max rank declared earlier
-   .brushOn(false)
-   .symbolSize(8)
-   .clipPadding(10)
-   .xAxisLabel("Lifetime Rank")
-   .yAxisLabel("Actual Score")
-   .title(function(d) {
-    return d.key[2] + " has an actual score of " + d.key[1] + " and ranks #" + d.key[0] + ". Character: " + d.key[4];
-   }) //Returns a description when a data-point on the graph is hovered over by user. Includes name, actual score, ranking and character. 
-   .colorAccessor(function(d) {
-    return d.key[3];
-   }) // Player's country is returned to help choose correct colour of mark. 
-   .colors(countryColors) // Colors used for countries are those declared earlier in countryColors
-   .dimension(rankDim) 
-   .group(actualScoresDim)
-   .margins({ top: 10, right: 10, bottom: 75, left: 70 });
+   // Plot a scatter graph
+   dc.scatterPlot("#Lifetime-rank-to-actual-score")
+    .width(1000)
+    .height(300)
+    .x(d3.scale.linear().domain([minRank, maxRank])) // x-axis scale depending upon min and max rank declared earlier
+    .brushOn(false)
+    .symbolSize(8)
+    .clipPadding(10)
+    .xAxisLabel("Lifetime Rank")
+    .yAxisLabel("Actual Score")
+    .title(function(d) {
+     return d.key[2] + " has an actual score of " + d.key[1] + " and ranks #" + d.key[0] + ". Character: " + d.key[4];
+    }) //Returns a description when a data-point on the graph is hovered over by user. Includes name, actual score, ranking and character. 
+    .colorAccessor(function(d) {
+     return d.key[3];
+    }) // Player's country is returned to help choose correct colour of mark. 
+    .colors(countryColors) // Colors used for countries are those declared earlier in countryColors
+    .dimension(rankDim)
+    .group(actualScoresDim)
+    .margins({ top: 10, right: 10, bottom: 75, left: 70 });
 
- };
+  };
 
- //Pie Chart Participation By Country
- this.show_participation_by_country = function(ndx) {
-// use dc to pluck information regarding a particular country
-  var country_dim = ndx.dimension(dc.pluck("Country"));
-  // group the information in order to give each country a "Total Lifetime Score" by totalling each lifetime score associated with it.
-  var total_lifetime_score = country_dim.group().reduceSum(dc.pluck("Lifetime Score"));
+  //Pie Chart Participation By Country
+  this.show_participation_by_country = function(ndx) {
+   // use dc to pluck information regarding a particular country
+   var country_dim = ndx.dimension(dc.pluck("Country"));
+   // group the information in order to give each country a "Total Lifetime Score" by totalling each lifetime score associated with it.
+   var total_lifetime_score = country_dim.group().reduceSum(dc.pluck("Lifetime Score"));
 
-  dc.pieChart("#lifetime-score-by-country")
-   .height(220)
-   .radius(100)
-   .transitionDuration(1500)
-   .dimension(country_dim)
-   .group(total_lifetime_score);
+   dc.pieChart("#lifetime-score-by-country")
+    .height(220)
+    .radius(100)
+    .transitionDuration(1500)
+    .dimension(country_dim)
+    .group(total_lifetime_score);
 
- };
+  };
 
- // Barchart for Lifetime scores by Character
- this.show_lifetime_scores_by_character = function(ndx) {
-  //Pluck all the different characters from the data, i.e. Guile, Chun Li etc
-  var character_dim = ndx.dimension(dc.pluck("Character"));
-  //Total each character's lifetime score.
-  var total_lifetime_score = character_dim.group().reduceSum(dc.pluck('Lifetime Score'));
+  // Barchart for Lifetime scores by Character
+  this.show_lifetime_scores_by_character = function(ndx) {
+   //Pluck all the different characters from the data, i.e. Guile, Chun Li etc
+   var character_dim = ndx.dimension(dc.pluck("Character"));
+   //Total each character's lifetime score.
+   var total_lifetime_score = character_dim.group().reduceSum(dc.pluck('Lifetime Score'));
 
-  dc.barChart("#total-lifetime-score-by-character")
-   .width(1000)
-   .height(300)
-   .margins({ top: 10, right: 50, bottom: 75, left: 75 })
-   .dimension(character_dim)
-   .group(total_lifetime_score)
-   .transitionDuration(500)
-   .x(d3.scale.ordinal())
-   .xUnits(dc.units.ordinal)
-   .elasticY(true)
-   .xAxisLabel("Characters")
-   .yAxisLabel("Lifetime Score")
-   .yAxis().ticks(4);
+   dc.barChart("#total-lifetime-score-by-character")
+    .width(1000)
+    .height(300)
+    .margins({ top: 10, right: 50, bottom: 75, left: 75 })
+    .dimension(character_dim)
+    .group(total_lifetime_score)
+    .transitionDuration(500)
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .elasticY(true)
+    .xAxisLabel("Characters")
+    .yAxisLabel("Lifetime Score")
+    .yAxis().ticks(4);
 
- };
- // Barchart for Average Lifetime scores by Character
- this.show_average_lifetime_score_per_character = function(ndx) {
-  // Pluck all the different characters from the data, i.e. Guile, Chun Li etc
-  var dim = ndx.dimension(dc.pluck('Character'));
-// Building custom reducers to get correct averages.
-  
-// Use custom reducers with reduce() method to group characters and find their averages.
-  var averageLifetimeScoreByCharacter = dim.group().reduce(add_item, remove_item, initialise);
+  };
+  // Barchart for Average Lifetime scores by Character
+  this.show_average_lifetime_score_per_character = function(ndx) {
+   // Pluck all the different characters from the data, i.e. Guile, Chun Li etc
+   var dim = ndx.dimension(dc.pluck('Character'));
+   
 
-  dc.barChart("#average-lifetime_score_by-character")
-   .width(1000)
-   .height(300)
-   .margins({ top: 10, right: 50, bottom: 75, left: 75 })
-   .dimension(dim)
-   .group(averageLifetimeScoreByCharacter)
-   // Return average lifetime score for character when hovered over, to 2 d.p.
-   .valueAccessor(function(d) {
-    return d.value.average.toFixed(2);
-   })
-   .transitionDuration(500)
-   .x(d3.scale.ordinal())
-   .xUnits(dc.units.ordinal)
-   .y(d3.scale.linear().domain([0, 170000]))
-   .xAxisLabel("Character")
-   .yAxisLabel("Average Lifetime Score")
-   .yAxis().ticks(4);
- };
+   // Use custom reducers with reduce() method to group characters and find their averages.
+   var averageLifetimeScoreByCharacter = dim.group().reduce(add_item, remove_item, initialise);
 
- // Bar Charts By Gender
- this.show_lifetime_scores_by_character_gender = function(ndx) {
-  // Pluck gender from data, i.e. M/F
-  var gender_dim = ndx.dimension(dc.pluck("Character Gender"));
-  // Total Lifetime scores for M/F characters
-  var total_lifetime_score = gender_dim.group().reduceSum(dc.pluck('Lifetime Score'));
+   dc.barChart("#average-lifetime_score_by-character")
+    .width(1000)
+    .height(300)
+    .margins({ top: 10, right: 50, bottom: 75, left: 75 })
+    .dimension(dim)
+    .group(averageLifetimeScoreByCharacter)
+    // Return average lifetime score for character when hovered over, to 2 d.p.
+    .valueAccessor(function(d) {
+     return d.value.average.toFixed(2);
+    })
+    .transitionDuration(500)
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .y(d3.scale.linear().domain([0, 170000]))
+    .xAxisLabel("Character")
+    .yAxisLabel("Average Lifetime Score")
+    .yAxis().ticks(4);
+  };
 
-  dc.barChart("#total-lifetime-score-by-character-gender")
-   .width(300)
-   .height(220)
-   .margins({ top: 10, right: 50, bottom: 75, left: 75 })
-   .dimension(gender_dim)
-   .group(total_lifetime_score)
-   .valueAccessor(function(d) {
-    return d.value;
-   })
-   .transitionDuration(500)
-   .x(d3.scale.ordinal())
-   .xUnits(dc.units.ordinal)
-   .elasticY(true)
-   .xAxisLabel("Character Gender")
-   .yAxisLabel("Total Lifetime Score")
-   .yAxis().ticks(4);
- };
+  // Bar Charts By Gender
+  this.show_lifetime_scores_by_character_gender = function(ndx) {
+   // Pluck gender from data, i.e. M/F
+   var gender_dim = ndx.dimension(dc.pluck("Character Gender"));
+   // Total Lifetime scores for M/F characters
+   var total_lifetime_score = gender_dim.group().reduceSum(dc.pluck('Lifetime Score'));
 
- this.show_average_lifetime_score_per_character_gender = function(ndx) {
-  var dim = ndx.dimension(dc.pluck('Character Gender'));
+   dc.barChart("#total-lifetime-score-by-character-gender")
+    .width(300)
+    .height(220)
+    .margins({ top: 10, right: 50, bottom: 75, left: 75 })
+    .dimension(gender_dim)
+    .group(total_lifetime_score)
+    .valueAccessor(function(d) {
+     return d.value;
+    })
+    .transitionDuration(500)
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .elasticY(true)
+    .xAxisLabel("Character Gender")
+    .yAxisLabel("Total Lifetime Score")
+    .yAxis().ticks(4);
+  };
 
-  
+  this.show_average_lifetime_score_per_character_gender = function(ndx) {
+   var dim = ndx.dimension(dc.pluck('Character Gender'));
+   // Reduce method with custom reducers as arguments, will produce average lifetime score for M/F characters
+   var averageLifetimeScoreByCharacterGender = dim.group().reduce(add_item, remove_item, initialise);
 
-  var averageLifetimeScoreByCharacter = dim.group().reduce(add_item, remove_item, initialise);
+   dc.barChart("#average-lifetime_score_by-character-gender")
+    .width(300)
+    .height(220)
+    .margins({ top: 10, right: 50, bottom: 75, left: 75 })
+    .dimension(dim)
+    .group(averageLifetimeScoreByCharacterGender)
+    // Return average lifetime score for gender when hovered over, to 2 d.p.
+    .valueAccessor(function(d) {
+     return d.value.average.toFixed(2);
+    }) 
+    .transitionDuration(500)
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .y(d3.scale.linear().domain([0, 120000]))
+    .xAxisLabel("Character Gender")
+    .yAxisLabel("Average Lifetime Score")
+    .yAxis().ticks(4);
+  };
 
-  dc.barChart("#average-lifetime_score_by-character-gender")
-   .width(300)
-   .height(220)
-   .margins({ top: 10, right: 50, bottom: 75, left: 75 })
-   .dimension(dim)
-   .group(averageLifetimeScoreByCharacter)
-   .valueAccessor(function(d) {
-    return d.value.average.toFixed(2);
-   })
-   .transitionDuration(500)
-   .x(d3.scale.ordinal())
-   .xUnits(dc.units.ordinal)
-   .y(d3.scale.linear().domain([0, 120000]))
-   .xAxisLabel("Character Gender")
-   .yAxisLabel("Average Lifetime Score")
-   .yAxis().ticks(4);
- };
-
-
-this.show_lifetime_scores_by_character(ndx);
+// Call the functions declared earlier
+  this.show_lifetime_scores_by_character(ndx);
   this.show_average_lifetime_score_per_character(ndx);
   this.show_participation_by_country(ndx);
   this.show_lifetime_rank_to_actual_scores_correlation(ndx);
   this.show_lifetime_scores_by_character_gender(ndx);
   this.show_average_lifetime_score_per_character_gender(ndx);
 
+// Render all the charts
   dc.renderAll();
-};
+ };
 };
 
 P = new StreetFighterApi;
+
+// Run Streetfighter Api 
 P.init();
